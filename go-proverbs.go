3b39c06734ce5b3d2c6f5e027c6d003ed481fa7b
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2016-10-04 16:27:50 +0200

    adds go-proverbs.go

---
+++ go-proverbs.go
@@ -0,0 +1,42 @@
+package goproverbs
+
+import "math/rand"
+
+var proverbs []string = []string{
+	"A little copying is better than a little dependency.",
+	"Cgo is not Go.",
+	"Cgo must always be guarded with build tags.",
+	"Channels orchestrate; mutexes serialize.",
+	"Clear is better than clever.",
+	"Concurrency is not parallelism.",
+	"Design the architecture, name the components, document the details.",
+	"Documentation is for users.",
+	"Don't communicate by sharing memory, share memory by communicating.",
+	"Don't just check errors, handle them gracefully.",
+	"Don't panic.",
+	"Errors are values.",
+	"Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.",
+	"Make the zero value useful.",
+	"Reflection is never clear.",
+	"Syscall must always be guarded with build tags.",
+	"The bigger the interface, the weaker the abstraction.",
+	"With the unsafe package there are no guarantees.",
+	"interface{} says nothing.",
+}
+
+func All() []string {
+	return proverbs
+}
+
+func Random() string {
+	i = rand.Intn(len(reasons))
+	return proverbs[i]
+}
+
+func First() string {
+	return proverbs[0]
+}
+
+func Last() string {
+	return proverbs[len(proverbs)]
+}
